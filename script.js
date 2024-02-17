8a0dec57cd61cd837bc4dbe9d2de65b5aa125f04
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2024-02-17 21:19:42 +0530

    feat:added js code

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let order_btn = document.getElementsByClassName('order_btn');
+const ordered_foods = document.querySelectorAll('.order_btn');
+
+ordered_foods.forEach(function(button){
+  button.addEventListener('click',function(){
+    const food_name = this.parentElement.querySelector('p').textContent;
+    addFoodIntoBox(food_name);
+  });
+});
+
+function addFoodIntoBox(food_name){
+  const orderedList = document.querySelector('.ordered_list ol');
+  const newList = document.createElement('li');
+  const newDiv = document.createElement('div');
+  const newFood = document.createElement('p');
+
+  newFood.textContent = food_name;
+  newDiv.classList.add('added_foods');
+  newDiv.appendChild(newFood);
+  newList.appendChild(newDiv);
+  orderedList.appendChild(newList);
+
+}
+
+
